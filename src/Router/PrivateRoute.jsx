74f6637c0d523e70670ce981c8227678bf0ad979
shoybit <shoyaib105@gmail.com>
2025-11-08 12:17:03 +0600

    customize r

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../Context/AuthContext";
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
-  if (loading) return <p className="text-center mt-10">Loading...</p>;
+  if (loading) return <p className="text-center mt-10"> plase Loading...</p>;
   if (!user) return <Navigate to="/login" />;
 
   return children;
